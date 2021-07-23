4a0cbdec9df1b0e04b229d6bf0900e660fb35eaf
John Henderson <john.henderson@energyweb.org>
2021-07-23 15:02:55 -0700

    CompanyIdentity and HumanIdentity tests

--- test/schema.test.ts
+++ test/schema.test.ts
@@ -5,21 +5,22 @@ import assetIDCredential from '../src/vc-schemas/AssetIDCredential.json';
 import assetSpecSchema from '../src/schema-def/AssetSpecSchema.json';
 import assetSpecCredential from '../src/vc-schemas/AssetSpecCredential.json';
 import humanIdentitySchema from '../src/schema-def/HumanIdentitySchema.json';
+import humanIdentityCredential from '../src/vc-schemas/HumanIdentityCredential.json';
 import companyIdentitySchema from '../src/schema-def/CompanyIdentitySchema.json';
+import companyIdentityCredential from '../src/vc-schemas/CompanyIdentityCredential.json';
 import timeSeriesSchema from '../src/schema-def/TimeSeriesSchema.json';
-import basicSchema from '../src/schema-def/BasicSchema.json';
 
 const ajv = new Ajv()
 addFormats(ajv)
 
-const verifyCredentialSubjectSchema = (schema: Schema, credential: { credentialSubject: object }) => {
-    const validate = ajv.compile(schema)
-    const valid = validate(credential.credentialSubject)
-    expect(valid).toBeTruthy()
+const verifyCredentialSubjectSchema = (schema: Schema, credential: { credentialSubject: Record<string, unknown> }) => {
+  const validate = ajv.compile(schema)
+  const valid = validate(credential.credentialSubject)
+  expect(valid).toBeTruthy()
 }
 
 describe("Issuance and verification tests", () => {
-  
+
   test("AssetID Schema", async () => {
     verifyCredentialSubjectSchema(assetIDSchema, assetIDCredential)
   });
@@ -28,49 +29,12 @@ describe("Issuance and verification tests", () => {
     verifyCredentialSubjectSchema(assetSpecSchema, assetSpecCredential)
   });
 
-  const basicData = {
-    "id": "abc:xyz:fdsjhkjkjhkjh"
-  }
-
-  
-  test("BasicSchema", async () => {
-    var validate = ajv.compile(basicSchema)
-    var valid = validate(basicData)
-    if (!valid) console.log(validate.errors)
+  test("CompanyIdentity", async () => {
+    verifyCredentialSubjectSchema(companyIdentitySchema, companyIdentityCredential)
   });
 
-  const companyIdentityData = {
-    "id": "did:example:corporation",
-    "type": "Corporation",
-    "legalName": "Elia Transmission Belgium",
-    "companyID": "0731.852.231",
-    "address": "1234 Apple Street",
-    "foundingDate": "1980-04-12",
-    "siegeSocialAddress": "1234 Apple Street"
-  }
-
-  
-  test("CompanyIdenty", async () => {
-    var validate = ajv.compile(companyIdentitySchema)
-    var valid = validate(companyIdentityData)
-    if (!valid) console.log("validate.errors")
-  });
-
-  const humanIdentityData = {
-    "id": "did:example:person",
-    "type": "Person",
-    "givenName": "Thierry",
-    "familyName": "Dupont",
-    "address": "1234 Apple Street",
-    "birthDate": "1980-04-12",
-    "nationalNumber": "12120112485"
-  }
-
-  
   test("HumanIdentity", async () => {
-    var validate = ajv.compile(humanIdentitySchema)
-    var valid = validate(humanIdentityData)
-    if (!valid) console.log(validate.errors)
+    verifyCredentialSubjectSchema(humanIdentitySchema, humanIdentityCredential)
   });
 
   const timeSeriesData = {
@@ -82,7 +46,7 @@ describe("Issuance and verification tests", () => {
     }
   }
 
-  
+
   test("TineSeries", async () => {
     var validate = ajv.compile(timeSeriesSchema)
     var valid = validate(timeSeriesData)
