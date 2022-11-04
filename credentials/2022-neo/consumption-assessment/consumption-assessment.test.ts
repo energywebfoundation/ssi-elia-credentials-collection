05fe0e11750041b0fb10be8175f621e5b6a6bd0a
John Henderson <john.henderson@energyweb.org>
2022-11-04 17:35:31 +0100

    test: update device-info and consumption-assess tests

--- credentials/2022-neo/consumption-assessment/consumption-assessment.test.ts
+++ credentials/2022-neo/consumption-assessment/consumption-assessment.test.ts
@@ -1,4 +1,4 @@
-import { Options, expand } from 'jsonld'
+import { Options, compact } from 'jsonld'
 import credential from './consumption-assessment-credential.json'
 import { issueAndVerify } from '../../../test/issue-and-verify'
 import { digitalBazaarDocumentLoader, transmuteDocumentLoader } from '../neo-document-loader'
@@ -12,11 +12,44 @@ describe("Consumption Assessment", () => {
     await issueAndVerify(credential, transmuteDocumentLoader)
   })
 
-  test("Credential expands as expected", async () => {
-    const options: Options.Expand = {
+  test("Compacted credential is as expected", async () => {
+    const options: Options.Compact = {
       documentLoader: digitalBazaarDocumentLoader
     }
-    const expanded = await expand(credential, options)
-    console.log(expanded)
+    const compacted = await compact(credential, {}, options)
+    const expected = {
+      "@id": "some URI, e.g. https://installer.example.com/credential/1",
+      "@type": [
+        "https://www.w3.org/2018/credentials#VerifiableCredential",
+        "https://vc-context.elia.be/2022/v1/PowerConsumingDeviceAssessmentCredential",
+      ],
+      "https://www.w3.org/2018/credentials#credentialSubject": {
+        "@id": "deviceIdScheme:123",
+        "@type": "https://vc-context.elia.be/2022/v1/AssessedPowerConsumingDevice",
+        "https://saref.etsi.org/saref4ener/powerSource": "mains3Phase",
+        "https://vc-context.elia.be/2022/v1/cutoffFrequencyConsumption": 15,
+        "https://vc-context.elia.be/2022/v1/cutoffVoltageConsumption": [
+          30,
+          30,
+        ],
+        "https://vc-context.elia.be/2022/v1/marginalConsumptionCosts": 5,
+        "https://vc-context.elia.be/2022/v1/maximumActivePowerConsumption": 40,
+        "https://vc-context.elia.be/2022/v1/maximumCurrentConsumption": 20,
+        "https://vc-context.elia.be/2022/v1/maximumReactivePowerConsumption": 40,
+        "https://vc-context.elia.be/2022/v1/maximumVoltageConsumption": 20,
+        "https://vc-context.elia.be/2022/v1/nominalActivePowerConsumption": 40,
+        "https://vc-context.elia.be/2022/v1/nominalCurrentConsumption": 10,
+        "https://vc-context.elia.be/2022/v1/nominalReactivePowerConsumption": 40,
+        "https://vc-context.elia.be/2022/v1/nominalVoltageConsumption": 10,
+      },
+      "https://www.w3.org/2018/credentials#issuanceDate": {
+        "@type": "http://www.w3.org/2001/XMLSchema#dateTime",
+        "@value": "2021-05-14T12:55:30Z",
+      },
+      "https://www.w3.org/2018/credentials#issuer": {
+        "@id": "did:example:dso",
+      },
+    }
+    expect(compacted).toEqual(expected)
   })
 })
--- credentials/2022-neo/device-info/device-info.test.ts
+++ credentials/2022-neo/device-info/device-info.test.ts
@@ -1,4 +1,4 @@
-import { Options, expand } from 'jsonld'
+import { Options, compact } from 'jsonld'
 import credential from './device-info-credential.json'
 import { issueAndVerify } from '../../../test/issue-and-verify'
 import { digitalBazaarDocumentLoader, transmuteDocumentLoader } from '../neo-document-loader'
@@ -12,11 +12,35 @@ describe("Device Info", () => {
     await issueAndVerify(credential, transmuteDocumentLoader)
   })
 
-  test("Credential expands as expected", async () => {
-    const options: Options.Expand = {
+  test("Compacted credential is as expected", async () => {
+    const options: Options.Compact = {
       documentLoader: digitalBazaarDocumentLoader
     }
-    const expanded = await expand(credential, options)
-    console.log(expanded)
+    const compacted = await compact(credential, {}, options)
+    const expected = {
+      "@id": "<some URI, e.g. https://elia.be/credential/1>",
+      "@type": [
+        "https://www.w3.org/2018/credentials#VerifiableCredential",
+        "https://vc-context.elia.be/2022/v1/DeviceInfoCredential",
+      ],
+      "https://www.w3.org/2018/credentials#credentialSubject": {
+        "@id": "deviceIdScheme:123",
+        "@type": "https://saref.etsi.org/saref4ener/Device",
+        "https://saref.etsi.org/saref4ener/brandName": "Device Manufacturer Inc",
+        "https://saref.etsi.org/saref4ener/deviceCode": "device 123",
+        "https://saref.etsi.org/saref4ener/deviceName": "Washing Machine 3000",
+        "https://saref.etsi.org/saref4ener/hardwareRevision": "revision1",
+        "https://saref.etsi.org/saref4ener/manufacturerDescription": "A next-gen washing machine",
+        "https://saref.etsi.org/saref4ener/manufacturerLabel": "WM-3000",
+      },
+      "https://www.w3.org/2018/credentials#issuanceDate": {
+        "@type": "http://www.w3.org/2001/XMLSchema#dateTime",
+        "@value": "2021-05-14T12:55:30Z",
+      },
+      "https://www.w3.org/2018/credentials#issuer": {
+        "@id": "did:example:dso",
+      },
+    }
+    expect(compacted).toEqual(expected)
   })
 })
