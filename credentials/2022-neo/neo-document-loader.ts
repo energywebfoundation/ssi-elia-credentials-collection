13f6fa0bd91ac4ae734140a72ed0845f0c06d9e4
John Henderson <john.henderson@energyweb.org>
2022-11-04 17:29:35 +0100

    fix(neo-doc-loader): remove neo-context.
    All credential specific contexts now

--- credentials/2022-neo/neo-document-loader.ts
+++ credentials/2022-neo/neo-document-loader.ts
@@ -1,5 +1,4 @@
 import didDoc from '../../test/fixtures/didDocument.json'
-import neoContext from './elia-neo-context.json'
 import consumptionAssessmentContext from './consumption-assessment/consumption-assessment-context.json'
 import deviceInfoContext from './device-info/device-info-context.json'
 import deviceLocationContext from './device-location/device-location-context.json'
@@ -8,7 +7,6 @@ import { JsonLd, RemoteDocument } from 'jsonld/jsonld-spec'
 import { baseContextMap } from '../../test/document-loader'
 
 const contextMap = Object.assign({
-    'https://vc-context.elia.be/2022/v1': neoContext,
     'https://vc-context.elia.be/2022/v1/consumption-assessment': consumptionAssessmentContext,
     'https://vc-context.elia.be/2022/v1/device-info': deviceInfoContext,
     'https://vc-context.elia.be/2022/v1/device-location': deviceLocationContext,
