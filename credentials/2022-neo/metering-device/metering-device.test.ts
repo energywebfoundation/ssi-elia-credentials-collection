309f53732f4665a60dab35cac32029999947e7fd
John Henderson <jrhender@users.noreply.github.com>
2022-11-14 14:49:04 -0700

    feat(metering-capable-device): add credential (#7)
    * feat(metering-capable-device): add credential
    
    * feat(metering-device): using saref:hasFunction
    
    * refactor(metering-device): remove "capable"
    
    * feat: current hasMeterReadingType
    
    * test(metering-device): fix test name

---
+++ credentials/2022-neo/metering-device/metering-device.test.ts
@@ -0,0 +1,50 @@
+import { Options, compact } from 'jsonld'
+import credential from './metering-device-credential.json'
+import { issueAndVerify } from '../../../test/issue-and-verify'
+import { digitalBazaarDocumentLoader, transmuteDocumentLoader } from '../neo-document-loader'
+
+describe("MeteringDevice", () => {
+  // test("Credential should match JSON Schema", async () => {
+  //   verifyCredentialSubjectSchema(schema, credential)
+  // })
+
+  test("Credential can be issued and verified", async () => {
+    await issueAndVerify(credential, transmuteDocumentLoader)
+  })
+
+  test("Compacted credential is as expected", async () => {
+    const options: Options.Compact = {
+      documentLoader: digitalBazaarDocumentLoader
+    }
+    const compacted = await compact(credential, {}, options)
+    const expected = {
+      "@id": "<some URI, e.g. https://elia.be/credential/1>",
+      "@type": [
+        "https://www.w3.org/2018/credentials#VerifiableCredential",
+        "https://vc-context.elia.be/2022/v1/MeteringCapableDeviceCredential",
+      ],
+      "https://www.w3.org/2018/credentials#credentialSubject": {
+        "@id": "deviceIdScheme:123",
+        "@type": "https://vc-context.elia.be/2022/v1/MeteringCapableDevice",
+        "https://saref.etsi.org/core/hasFunction": {
+          "@type": "https://saref.etsi.org/core/MeteringFunction",
+          "https://saref.etsi.org/core/hasMeterReadingType": {
+            "@type": "https://vc-context.elia.be/2022/v1/Current",
+          },
+        },
+        "https://vc-context.elia.be/2022/v1/areDataAggregated": true,
+        "https://vc-context.elia.be/2022/v1/numSecondsOfAggregation": 60,
+        "https://vc-context.elia.be/2022/v1/precisionClassOfMeter": "precise",
+        "https://vc-context.elia.be/2022/v1/typeOfMetering": "voltage",
+      },
+      "https://www.w3.org/2018/credentials#issuanceDate": {
+        "@type": "http://www.w3.org/2001/XMLSchema#dateTime",
+        "@value": "2021-05-14T12:55:30Z",
+      },
+      "https://www.w3.org/2018/credentials#issuer": {
+        "@id": "did:example:dso",
+      },
+    }
+    expect(compacted).toEqual(expected)
+  })
+})
