f5d987d14d27f35e5d9b3c7a9a010a89f9f7d8dc
John Henderson <jrhender@users.noreply.github.com>
2022-12-12 13:30:11 +0100

    feat(device-info): add json schema (#17)

--- credentials/2022-neo/device-info/device-info.test.ts
+++ credentials/2022-neo/device-info/device-info.test.ts
@@ -1,14 +1,16 @@
 import { Options, compact } from 'jsonld'
 import credential from './device-info-credential.json'
 import dataDisplay from './device-info-data-display.json'
+import schema from './device-info-schema.json'
 import { issueAndVerify } from '../../../test/issue-and-verify'
 import { digitalBazaarDocumentLoader, transmuteDocumentLoader } from '../neo-document-loader'
 import { verifyDataDisplayStructure } from '../../../test/verify-data-display-structure'
+import { verifyCredentialSubjectSchema } from '../../../test/verify-credential-subject-schema'
 
 describe("Device Info", () => {
-  // test("Credential should match JSON Schema", async () => {
-  //   verifyCredentialSubjectSchema(schema, credential)
-  // })
+  test("Credential should match JSON Schema", async () => {
+    verifyCredentialSubjectSchema(schema, credential)
+  })
 
   test("Credential can be issued and verified", async () => {
     await issueAndVerify(credential, transmuteDocumentLoader)
@@ -27,7 +29,7 @@ describe("Device Info", () => {
       ],
       "https://www.w3.org/2018/credentials#credentialSubject": {
         "@id": "deviceIdScheme:123",
-        "@type": "https://saref.etsi.org/saref4ener/Device",
+        "@type": "https://vc-context.elia.be/2022/v1/Device",
         "https://saref.etsi.org/saref4ener/brandName": "Device Manufacturer Inc",
         "https://saref.etsi.org/saref4ener/deviceCode": "device 123",
         "https://saref.etsi.org/saref4ener/deviceName": "Washing Machine 3000",
