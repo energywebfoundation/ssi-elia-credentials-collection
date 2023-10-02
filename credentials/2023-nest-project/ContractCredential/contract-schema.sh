36ae2c37f064fa6155f0e98db346c93deb59d123
John Henderson <john.henderson@energyweb.org>
2023-10-02 09:30:12 +0100

    actually include Person + add Contract type

--- credentials/2023-nest-project/ContractCredential/contract-schema.sh
+++ credentials/2023-nest-project/ContractCredential/contract-schema.sh
@@ -5,6 +5,7 @@ COMMANDS=(
   "tldrc -i contract.tldr json-schema https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/ContractParty"
   "tldrc -i contract.tldr json-schema https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/Contract"
   "tldrc -i contract.tldr json-schema https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/Device"
+  "tldrc -i contract.tldr json-schema https://schema.org/Person"
   "tldrc -i contract.tldr json-schema https://schema.org/Organization"
 )
 
@@ -12,6 +13,7 @@ FILES=(
   "schemas/contract-party-schema.json"
   "schemas/contract-schema.json"
   "schemas/device-schema.json"
+  "schemas/person-schema.json"
   "schemas/organization-schema.json"
 )
 
--- credentials/2023-nest-project/ContractCredential/contract.test.ts
+++ credentials/2023-nest-project/ContractCredential/contract.test.ts
@@ -4,6 +4,7 @@ import credential from "./contract-credential.json"
 import contractPartySchema from "./schemas/contract-party-schema.json"
 import contractSchema from "./schemas/contract-schema.json"
 import organizationSchema from "./schemas/organization-schema.json"
+import personSchema from "./schemas/person-schema.json"
 import deviceSchema from "./schemas/device-schema.json"
 import { issueAndVerify } from "../../../test/issue-and-verify"
 import {
@@ -19,6 +20,7 @@ describe("Contract", () => {
       contractSchema,
       organizationSchema,
       deviceSchema,
+      personSchema,
     ])
   })
 
@@ -35,7 +37,7 @@ describe("Contract", () => {
       "@id": "<some URI, e.g. https://example.com/credential/1>",
       "@type": [
         "https://www.w3.org/2018/credentials#VerifiableCredential",
-        "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/credentials/2023-nest-project/ContractCredential",
+        "ContractPartyCredential",
       ],
       "https://www.w3.org/2018/credentials#credentialSubject": {
         "@id": "did:example:subject",
@@ -53,9 +55,13 @@ describe("Contract", () => {
               {
                 "@id": "deviceIdScheme:123",
                 "@type":
-                  "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/credentials/2023-nest-project/Device",
+                  "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/Device",
               },
-            "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/counterParty":
+            "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/expressionOfTheOffer":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+            "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/jurisdiction":
+              "Courts of Bruxelles",
+            "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/party1":
               {
                 "@type": "https://schema.org/Organization",
                 "https://schema.org/address":
@@ -63,10 +69,14 @@ describe("Contract", () => {
                 "https://schema.org/brand": "Car Manufacturer Inc",
                 "https://schema.org/iso6523Code": "123456789",
               },
-            "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/expressionOfTheOffer":
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-            "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/jurisdiction":
-              "Courts of Bruxelles",
+            "https://github.com/energywebfoundation/elia-energyblocks-vcs/tree/master/ontology/v1/party2":
+              {
+                "@type": "https://schema.org/Person",
+                "https://schema.org/address":
+                  "Boulevard de l'Empereur, 1000 Bruxelles",
+                "https://schema.org/familyName": "Doe",
+                "https://schema.org/givenName": "John",
+              },
           },
       },
       "https://www.w3.org/2018/credentials#issuanceDate": {
